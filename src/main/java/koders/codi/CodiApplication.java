6afc66d87514118842da38e70b4f5e957b09d00d
sor999 <sorsor999@naver.com>
2024-01-25 14:18:34 +0900

    feat: add init & DB setting

---
+++ src/main/java/koders/codi/CodiApplication.java
@@ -0,0 +1,13 @@
+package koders.codi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koders/codi/CodiApplicationTests.java
@@ -0,0 +1,13 @@
+package koders.codi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
