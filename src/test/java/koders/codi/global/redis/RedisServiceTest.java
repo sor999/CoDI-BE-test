3c237eb34f0bdaac737e57045bd366a5ae422322
sor999 <sorsor999@naver.com>
2024-02-10 22:51:33 +0900

    feat: DockerFile 설정

--- src/test/java/koders/codi/global/redis/RedisServiceTest.java
+++ src/test/java/koders/codi/global/redis/RedisServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
 @SpringBootTest
 class RedisServiceTest {
     @Autowired
