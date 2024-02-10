4162f8eaafae249805cc10a43bb9daf3ea93b2ce
sor999 <sorsor999@naver.com>
2024-02-10 22:50:43 +0900

    feat: 개발 환경 분리

--- src/test/java/koders/codi/CodiApplicationTests.java
+++ src/test/java/koders/codi/CodiApplicationTests.java
@@ -1,9 +1,15 @@
 package koders.codi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CodiApplicationTests {
 
 	@Test
